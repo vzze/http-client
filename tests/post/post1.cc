97c3aa6785c3d1f7f1140f153a5ef3abf62497fd
vzze <lolo32778@gmail.com>
2023-06-30 03:12:47 +0300

    verbose output

--- tests/post/post1.cc
+++ tests/post/post1.cc
@@ -2,5 +2,5 @@
 #include <iostream>
 
 int main() {
-    std::cout << req::send(req::server{L"http://httpbin.org/post"}, req::request(L"POST", "req=none")) << '\n';
+    std::clog << req::send(req::server{L"http://httpbin.org/post"}, req::request(L"POST", "req=none")) << '\n';
 }
